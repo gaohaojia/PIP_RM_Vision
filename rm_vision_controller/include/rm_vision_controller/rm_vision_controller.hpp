df63684b21d220430e9b216958bda6ca2a48c12d
gaohaojia <2723687563@qq.com>
2024-04-09 20:16:09 +0800

    first commit

---
+++ rm_vision_controller/include/rm_vision_controller/rm_vision_controller.hpp
@@ -0,0 +1,74 @@
+// Copyright (c) 2022 ChenJun
+// Licensed under the Apache-2.0 License.
+
+#ifndef RM_VISION_CONTROLLER__RM_VISION_CONTROLLER_HPP_
+#define RM_VISION_CONTROLLER__RM_VISION_CONTROLLER_HPP_
+
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <serial_interfaces/msg/referee.hpp>
+#include <auto_aim_interfaces/msg/target.hpp>
+#include <serial_interfaces/msg/vision_recv.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/subscription.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+// C++ system
+
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+namespace rm_vision_controller
+{
+class RMVisionController : public rclcpp::Node
+{
+public:
+  explicit RMVisionController(const rclcpp::NodeOptions & options);
+
+private:
+
+  void receiveDataVision(serial_interfaces::msg::VisionRecv::SharedPtr msg);
+
+  void sendDataVision(auto_aim_interfaces::msg::Target::SharedPtr msg);
+
+  void setParam(const rclcpp::Parameter & param);
+
+  void resetTracker();
+
+  // Param client to set detect_colr
+  using ResultFuturePtr = std::shared_future<std::vector<rcl_interfaces::msg::SetParametersResult>>;
+  bool initial_set_param_ = false;
+  uint8_t previous_receive_color_ = 0;
+  rclcpp::AsyncParametersClient::SharedPtr detector_param_client_;
+  ResultFuturePtr set_param_future_;
+
+  // Service client to reset tracker
+  rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr reset_tracker_client_;
+
+  // Aimimg point receiving from serial port for visualization
+  visualization_msgs::msg::Marker aiming_point_;
+
+  // Broadcast tf from odom to gimbal_link
+  double timestamp_offset_ = 0;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+  rclcpp::Subscription<auto_aim_interfaces::msg::Target>::SharedPtr target_sub_;
+  rclcpp::Subscription<serial_interfaces::msg::VisionRecv>::SharedPtr vision_recv_sub_;
+
+  // For debug usage
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr latency_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+
+  std::thread receive_thread_;
+};
+}  // namespace rm_vision_controller
+
+#endif  // RM_VISION_CONTROLLER__RM_VISION_CONTROLLER_HPP_
---
+++ rm_vision_controller/src/rm_vision_controller.cpp
@@ -0,0 +1,161 @@
+// Copyright (c) 2022 ChenJun
+// Licensed under the Apache-2.0 License.
+
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <rclcpp/logging.hpp>
+#include <rclcpp/qos.hpp>
+#include <rclcpp/utilities.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+// C++ system
+#include <cstdint>
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rm_vision_controller/crc.hpp"
+#include "rm_vision_controller/packet.hpp"
+#include "rm_vision_controller/rm_vision_controller.hpp"
+
+namespace rm_vision_controller
+{
+RMVisionController::RMVisionController(const rclcpp::NodeOptions & options)
+: Node("rm_vision_controller", options)
+{
+  RCLCPP_INFO(get_logger(), "Start RMVisionController!");
+
+  // TF broadcaster
+  timestamp_offset_ = this->declare_parameter("timestamp_offset", 0.0);
+  tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+  // Create Publisher
+  latency_pub_ = this->create_publisher<std_msgs::msg::Float64>("/latency", 10);
+  marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("/aiming_point", 10);
+
+  // Detect parameter client
+  detector_param_client_ = std::make_shared<rclcpp::AsyncParametersClient>(this, "armor_detector");
+
+  // Tracker reset service client
+  reset_tracker_client_ = this->create_client<std_srvs::srv::Trigger>("/tracker/reset");
+
+  aiming_point_.header.frame_id = "odom";
+  aiming_point_.ns = "aiming_point";
+  aiming_point_.type = visualization_msgs::msg::Marker::SPHERE;
+  aiming_point_.action = visualization_msgs::msg::Marker::ADD;
+  aiming_point_.scale.x = aiming_point_.scale.y = aiming_point_.scale.z = 0.12;
+  aiming_point_.color.r = 1.0;
+  aiming_point_.color.g = 1.0;
+  aiming_point_.color.b = 1.0;
+  aiming_point_.color.a = 1.0;
+  aiming_point_.lifetime = rclcpp::Duration::from_seconds(0.1);
+
+  // Create Subscription
+  target_sub_ = this->create_subscription<auto_aim_interfaces::msg::Target>(
+    "/tracker/target", rclcpp::SensorDataQoS(),
+    std::bind(&RMVisionController::sendDataVision, this, std::placeholders::_1));
+  vision_recv_sub_ = this->create_subscription<serial_interfaces::msg::VisionRecv>(
+    "vision_recv", 10, std::bind(&RMVisionController::receiveDataVision, this, std::placeholders::_1));
+}
+
+void RMVisionController::receiveDataVision(const serial_interfaces::msg::VisionRecv::SharedPtr msg)
+{
+  ReceivePacket packet;
+  packet.detect_color = msg->detect_color;
+  packet.reset_tracker = msg->reset_tracker;
+  packet.roll = msg->roll;
+  packet.pitch = msg->pitch;
+  packet.yaw = msg->yaw;
+  packet.aim_x = msg->aim_x;
+  packet.aim_y = msg->aim_y;
+  packet.aim_z = msg->aim_z;
+
+  // RCLCPP_INFO(get_logger(), "R:%f, P:%f, Y:%f", packet.roll, packet.pitch, packet.yaw);
+
+  if (!initial_set_param_ || packet.detect_color != previous_receive_color_) {
+    setParam(rclcpp::Parameter("detect_color", packet.detect_color));
+    previous_receive_color_ = packet.detect_color;
+  }
+
+  if (packet.reset_tracker) {
+    resetTracker();
+  }
+
+  geometry_msgs::msg::TransformStamped t;
+  timestamp_offset_ = this->get_parameter("timestamp_offset").as_double();
+  t.header.stamp = this->now() + rclcpp::Duration::from_seconds(timestamp_offset_);
+  t.header.frame_id = "odom";
+  t.child_frame_id = "gimbal_link";
+  tf2::Quaternion q;
+  q.setRPY(packet.roll, packet.pitch, packet.yaw);
+  t.transform.rotation = tf2::toMsg(q);
+  tf_broadcaster_->sendTransform(t);
+
+  if (abs(packet.aim_x) > 0.01) {
+    aiming_point_.header.stamp = this->now();
+    aiming_point_.pose.position.x = packet.aim_x;
+    aiming_point_.pose.position.y = packet.aim_y;
+    aiming_point_.pose.position.z = packet.aim_z;
+    marker_pub_->publish(aiming_point_);
+  }
+}
+
+void RMVisionController::sendDataVision(const auto_aim_interfaces::msg::Target::SharedPtr msg)
+{
+  try {
+    std_msgs::msg::Float64 latency;
+    latency.data = (this->now() - msg->header.stamp).seconds() * 1000.0;
+    RCLCPP_DEBUG_STREAM(get_logger(), "Total latency: " + std::to_string(latency.data) + "ms");
+    latency_pub_->publish(latency);
+  } catch (const std::exception & ex) {
+    RCLCPP_ERROR(get_logger(), "Error while sending data: %s", ex.what());
+  }
+}
+
+void RMVisionController::setParam(const rclcpp::Parameter & param)
+{
+  if (!detector_param_client_->service_is_ready()) {
+    RCLCPP_WARN(get_logger(), "Service not ready, skipping parameter set");
+    return;
+  }
+
+  if (
+    !set_param_future_.valid() ||
+    set_param_future_.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+    RCLCPP_INFO(get_logger(), "Setting detect_color to %ld...", param.as_int());
+    set_param_future_ = detector_param_client_->set_parameters(
+      {param}, [this, param](const ResultFuturePtr & results) {
+        for (const auto & result : results.get()) {
+          if (!result.successful) {
+            RCLCPP_ERROR(get_logger(), "Failed to set parameter: %s", result.reason.c_str());
+            return;
+          }
+        }
+        RCLCPP_INFO(get_logger(), "Successfully set detect_color to %ld!", param.as_int());
+        initial_set_param_ = true;
+      });
+  }
+}
+
+void RMVisionController::resetTracker()
+{
+  if (!reset_tracker_client_->service_is_ready()) {
+    RCLCPP_WARN(get_logger(), "Service not ready, skipping tracker reset");
+    return;
+  }
+
+  auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+  reset_tracker_client_->async_send_request(request);
+  RCLCPP_INFO(get_logger(), "Reset tracker!");
+}
+
+}  // namespace rm_vision_controller
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(rm_vision_controller::RMVisionController)
